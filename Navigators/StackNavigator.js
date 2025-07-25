b551d9f99e988e178539240bafdb9a737b9c653b
immahmoodpasha <119700375+immahmoodpasha@users.noreply.github.com>
2025-07-25 23:38:26 +0530

    New directory

--- Navigators/StackNavigator.js
+++ Navigators/StackNavigator.js
@@ -5,19 +5,14 @@ import Home from '../screens/Home';
 import Signin from '../screens/Signin';
 import Categories from '../screens/Categories';
 
-
-
-
 const StackNavigator = () => {
    const Stack = createNativeStackNavigator();
   return (
-    
-    <Stack.Navigator initialRouteName="Categories" screenOptions={{headerShown: false}}>
+    <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
       <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Signup" component={Signup} />
-      <Stack.Screen name="Signin" component={Signin} />
-      <Stack.Screen name="Categories" component={Categories} />
-      {/* Add more screens as needed */}
+      <Stack.Screen name="Signup" component={Signup}/>
+      <Stack.Screen name="Signin" component={Signin}/>
+      <Stack.Screen name="Categories" component={Categories}/>
     </Stack.Navigator>
   );
 }
--- screens/Signin.js
+++ screens/Signin.js
@@ -1,7 +1,10 @@
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import { useNavigation } from '@react-navigation/native';
+import AuthInput from '../components/AuthInput';
+import AuthButton from '../components/AuthButton';
+import sharedStyles from '../styles/authStyles';
 
 const Signin = () => {
   const navigation = useNavigation();
@@ -9,94 +12,38 @@ const Signin = () => {
   return (
     <View>
       <Icon name="box-open" size={50} color="#8404aeff" style={{ alignSelf: 'center', marginTop: 50 }} />
-      <Text style={styles.heading}>Shopsy Login</Text>
+      <Text style={sharedStyles.heading}>Shopsy Login</Text>
 
-      <View style={styles.InputContainer}>
-        <Text style={styles.label}>Email</Text>
-        <TextInput
-          style={styles.InputField}
-          placeholder='Enter your email'
-          keyboardType='email-address'
-        />
+      <View style={sharedStyles.container}>
+        <AuthInput label="Email" placeholder="Enter your email" keyboardType="email-address" />
+        <AuthInput label="Password" placeholder="Enter your password" secureTextEntry />
 
-        <Text style={styles.label}>Password</Text>
-        <TextInput
-          style={styles.InputField}
-          placeholder='Enter your password'
-          secureTextEntry={true}
-        />
-        <Text style={styles.forgotPassword}>forgot password?</Text>
+        <Text style={styles.forgotPassword}>Forgot password?</Text>
 
-        <TouchableOpacity
-        onPress={() => navigation.navigate('Categories')}
-        activeOpacity={0.7}
-        style={styles.SigninButton}>
-          <Text style={styles.SigninText}>Sign In</Text>
-        </TouchableOpacity>
-        
+        <AuthButton title="Sign In" onPress={() => navigation.navigate('Categories')} />
       </View>
-      
 
-      <Text style={{ textAlign: 'center', marginTop: 20 }}>
+      <Text style={styles.footer}>
         Don't have an account?{' '}
-        <Text 
-          onPress={() => navigation.navigate('Signup')} 
-          style={{ color: '#8404aeff' }}
-        >
+        <Text style={styles.link} onPress={() => navigation.navigate('Signup')}>
           Sign Up
         </Text>
-        
       </Text>
-      
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  heading: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#8404aeff',
-    marginTop: 10,
-    textAlign: 'center',
-  },
-  InputField: {
-    marginVertical: 10,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    padding: 10,
-    borderRadius: 5,
-  },
-  InputContainer: {
-    margin: 30,
-    marginTop: 60,
-    backgroundColor: '#ffffffff',
-    padding: 20,
-    borderRadius: 10,
-    shadowColor: '#000',
-  },
-  label: {
-    fontWeight: 'bold',
-    marginLeft: 5,
-  },
-  SigninButton: {
-    backgroundColor: '#8404aeff',
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 20,
-    width: '80%',
-    marginLeft: '10%',
-    marginBottom: 20,
-  },
-  SigninText: {
-    color: '#fff',
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontSize: 18,
-  },
   forgotPassword: {
     textAlign: 'right',
     marginTop: 10,
+    color: '#8404aeff',
+  },
+  footer: {
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  link: {
     color: '#8404aeff',
   },
 });
