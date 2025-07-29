c78f6e2a6154f3a327dd4722fbbd677442d2bb5b
Koushik1403 <91865318+Koushik1403@users.noreply.github.com>
2025-07-29 11:34:21 +0530

    OrderHistory Updated

--- Navigators/StackNavigator.js
+++ Navigators/StackNavigator.js
@@ -6,19 +6,24 @@ import Signin from '../screens/Signin';
 import Categories from '../screens/Categories';
 import Dashboard from '../screens/Dashboard';
 import OrderHistory from '../screens/OrderHistory';
+import OrderSummary from '../screens/OrderSummary'
+import UserProfile from '../screens/UserProfile';
+
 
 
 const StackNavigator = () => {
    const Stack = createNativeStackNavigator();
   return (
     
-    <Stack.Navigator initialRouteName="Dashboard" screenOptions={{headerShown: false}}>
+    <Stack.Navigator initialRouteName="OrderHistory" screenOptions={{headerShown: false}}>
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Signup" component={Signup} />
       <Stack.Screen name="Signin" component={Signin} />
       <Stack.Screen name="Categories" component={Categories} />
       <Stack.Screen name="Dashboard" component={Dashboard}/>
       <Stack.Screen name="OrderHistory" component={OrderHistory}/>
+      <Stack.Screen name="OrderSummary" component={OrderSummary}/>
+      <Stack.Screen name="UserProfile" component={UserProfile}/>
 
       {/* Add more screens as needed */}
     </Stack.Navigator>
--- components/OrderHistoryCard.js
+++ components/OrderHistoryCard.js
@@ -9,6 +9,7 @@ import { useState, useEffect } from "react";
 function OrderHistoryCard ({orders}){
     const navigation = useNavigation()
     return(
+        <TouchableOpacity onPress={() => navigation.navigate('OrderSummary',{order:orders})} activeOpacity={0.8}>
         <View style={styles.orderscard}>
             <View style={styles.orderscardheader}>
                 <Text style={{fontSize:15,fontWeight:550}}>{orders.id}</Text>
@@ -24,9 +25,9 @@ function OrderHistoryCard ({orders}){
                 }
 
             </View>
-            <View>
-                {orders.items.map((item, index) => (
-                    <View key={index} style={styles.orderscardbody}>
+            <View style={{marginTop:10}}>
+                {orders.items.map((item) => (
+                    <View key={item.name} style={styles.orderscardbody}>
                         <View style={styles.OrderHistoryQuantityDiv}>
                             <Text>X {item.quantity}</Text>
                         </View>
@@ -37,24 +38,27 @@ function OrderHistoryCard ({orders}){
                     ))}
             </View>
             <View style={{borderBottomColor:'grey', borderBottomWidth:0.40, borderStyle:'dashed'}}></View>
-            <View style={{display:'flex', justifyContent:"center", alignItems:'center'}}>
+            {/* <View style={{display:'flex', justifyContent:"center", alignItems:'center'}}>
                 <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('OrderSummary',{order:orders})}>
                     <Text style={{fontSize:15, fontWeight:400}}>View Order</Text>
                  </TouchableOpacity>
-            </View>
-            <View style={{display:'flex', justifyContent:'center', alignItems:'center',marginBottom:10}}>
-                <Text style={{fontSize:15, fontWeight:300}}>Ordered:
-                     {orders.date},{orders.time} . Bill Total: ₹{orders.billing.totalBill} </Text>
+            </View> */}
+            <View style={{display:'flex', justifyContent:'center', alignItems:'center',marginBottom:10, marginTop:10}}>
+                <Text style={{fontSize:15, fontWeight:300, color:'grey'}}>Ordered:
+                     {orders.orderplaced},{orders.orderplacedtime} . Bill Total: ₹{orders.billing.totalBill} </Text>
             </View>
         </View>
+        </TouchableOpacity>
     )
 }
 
 const styles = StyleSheet.create({
     orderscard: {
         margin:10,
+        marginLeft:20,
+        marginRight:20,
         borderWidth: 0.16,
-        borderRadius:8,
+        borderRadius:10,
         backgroundColor:'white'
     },
     orderscardheader: {
@@ -63,9 +67,9 @@ const styles = StyleSheet.create({
         alignItems:'center',
         justifyContent:'space-between',
         height:40,
-        backgroundColor:'#a9a9a914',
+        backgroundColor:'#7e55b12d',
         padding:7,
-        borderRadius:8
+        borderRadius:10,
     },
     orderscardbody: {
         marginTop:5,
@@ -77,7 +81,7 @@ const styles = StyleSheet.create({
         gap:10
     },
     OrderHistoryQuantityDiv: {
-        backgroundColor:'#a9a9a928', 
+        backgroundColor:'#595e9c0d', 
         width:35, 
         height:25, 
         display:'flex', 
@@ -93,7 +97,7 @@ const styles = StyleSheet.create({
         display:'flex',
         alignItems:'center',
         justifyContent:'center',
-        backgroundColor:'#a2a2a26a'
+        backgroundColor:'#7e55b154'
     }
 }
 )
